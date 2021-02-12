664ec18a5cb0a407e982dd2233267a473b9a8edc
Diogo <40742985+Diogomes@users.noreply.github.com>
2021-02-12 10:54:30 -0300

    Update classesEobjetos.java

--- classesEobjetos.java
+++ classesEobjetos.java
@@ -1,1 +1,9 @@
 // classe é o objeto que será utilizado exemplo caneta
+// as classes recebem atributos e:
+// Classe Caneta
+//  Modelo caractere #atributo1
+//  Cor  caractere    #atributo2
+//  Ponta Real  #atributo3
+//  Carga  Inteiro #atributo4
+// Tampa Logico    #atributo5
+// Metodo rabiscar()# Método é o que faz o objeto, entre parenteses estará os parâmetros (O QUE FAZ O OBJETO)
