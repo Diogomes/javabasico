a8c00b259e1c06b063f798e5bed809ba99f70fb1
Diogomes <gomes.d.n05@gmail.com>
2021-03-15 08:24:23 -0300

    execicio
    add exercícios

---
+++ capitulo_02/src/inicial/Exercicios.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package inicial;
+import java.util.Scanner;
+
+
+/**
+ * @author dngom
+ *
+ */
+public class Exercicios {
+
+	/**
+	 * @param args
+	 */
+	public static void main ( String[] args){//Exercicio 2.3, 2.4, 2.5, 2.6
+	    Scanner input = new Scanner( System.in);
+	    
+	  int c;
+	  int thisIsAVariable;
+	  int q76354;
+	  int numero;
+	  int x;
+	  int y;
+	  int z;
+	  int resultado;
+	 
+	  
+	  System.out.printf("Insira um inteiro :" );
+	  numero = input.nextInt();
+	  
+	  System.out.printf("Insira um inteiro ao valor de c :" );
+	  c = input.nextInt();
+	  
+	  
+	  System.out.printf("Insira o valor inteiro de x :" );
+	  x = input.nextInt();
+	  
+	  System.out.printf("Insira o valor inteiro de y :" );
+	  y = input.nextInt();
+	  
+	  System.out.printf("Insira o valor inteiro de z :" );
+	  z = input.nextInt();
+	  
+	  resultado = x * y * z;
+	  
+	  System.out.printf("Resultado é %d%n", resultado);
+	  
+	  
+	  if (numero < 7);
+	       System.out.println("Número é menor que 7");
+	       
+	  if (numero >= 7); //
+	      System.out.println("Número é igual ou maior que 7");
+	  
+	  if (c < 7);
+	      System.out.println("C é menor que 7");
+	      
+	    
+	      
+	      System.out.println("Esse é um programa em java");
+	      System.out.println(" Ese é um Programa \n Em java");
+	      System.out.printf("%s\n%s\n", "Ese é um programa","Em java");
+		// TODO Auto-generated method stub
+
+	}
+
+}
