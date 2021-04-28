9d9908490dc06eb5370f1f51acb7020778a273ed
Diogo <40742985+Diogomes@users.noreply.github.com>
2021-04-28 09:35:54 -0300

    classes, objetos e acesso a atributos

--- Teoria/classesEobjetos.java
+++ Teoria/classesEobjetos.java
@@ -8,3 +8,34 @@
 // Tampa Logico    #atributo5
 // Metodo rabiscar()# Método é o que faz o objeto, entre parenteses estará os parâmetros (O QUE FAZ O OBJETO).
 
+//As entidades que colaboram na funcionalidade de um sistema são suas classes, definição muito técnica
+
+//classes são abstrações de um objeto
+//exemplo existem várias formas de cadeira e como são formadas o objeto cadeira
+// cadeira com rodas, com 3 pés, de plastico, ferro etc.
+
+//Objeto é uma instãncia concreta da classe
+
+//classe cria ou instancia um objeto e cada objeto são únicos mesmo sendo do mesmo tipo
+// classe é abstrata e o objeto é concreto.
+// O estado de uma classe pode ser representado por Atributos que são características da classe
+//O comportamento de uma classe pode ser representado por Método  que é o que a classe oide fazer
+
+//ou seja classe é definida pelo seu estado (atributos) e seu comportamento (métodos)
+
+//acessando atributos!
+
+//public class carro{ classe carro
+    //int potencia; atributo potencia
+    //float velocidade; atributo velocidade
+
+//}
+
+//Criando Objetos a partir da classe.
+
+//Carro(declara a variavel) classic(o classic é uma nova instãncia do tipo carro) = new Carro (); (new operador para criar novo objeto)
+
+// acessando atributos
+
+//classic.potencia =3;
+//float v = classic.velocidade;
